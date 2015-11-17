89b5af48d235cb8624aae8433871ae720fac9c63
Yuriy Miroshnyk <y.miroshnyk@gmail.com>
2015-11-17 22:21:11 +0200

    Initial Dockerfile.

---
+++ entry_point.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+_cleanup() { 
+	echo "Stopping services..." 
+
+	service avreg stop
+	service apache2 stop
+	service mysql stop
+
+	kill -s SIGTERM $!
+
+	echo "...container stopped."
+                                                 
+	exit 0
+}
+
+echo "Starting services..." 
+
+# remove any ghost service pids in case if container was incorrectly killed 
+service avreg stop
+service apache2 stop
+service mysql stop
+
+service mysql start
+service apache2 start
+service avreg start
+
+echo "...services started."
+
+trap _cleanup SIGTERM
+trap _cleanup SIGINT
+
+while [ 1 ]
+do                                                                         
+  sleep 60 &                                                             
+  wait $!                                                                
+done
